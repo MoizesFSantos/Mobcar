363cddbd3be821222e155b863562cd701182744a
MoizesFSantos <moizesferreira66@hotmail.com>
2022-06-07 13:36:16 -0300

    first commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:mobcar/shared/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          'MOBCAR',
+          style: TextStyle(color: pColor, fontWeight: FontWeight.w100),
+        ),
+        actions: [
+          Builder(builder: (BuildContext context) {
+            return IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu_open_rounded,
+                color: pColor,
+              ),
+            );
+          })
+        ],
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        color: Colors.transparent,
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '2020. All rights reserved to Mobacar.',
+                style: TextStyle(
+                    color: pColor, fontSize: 16.0, fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const pColor = Color.fromARGB(
+  255,
+  0,
+  173,
+  238,
+);
